03ef71931506889e6d0f95bd282d1b8530128dc7
Amina20040 <113467210+Amina20040@users.noreply.github.com>
2022-09-13 20:12:52 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form action="login.php" method="post" >
+        <h2>LOGIN<h2>
+        
+        <label>User Name</label>
+        <input type="text" name="nomprenomcl" placeholder="User Name"><br>
+        
+        <label>password</label>
+        <input type="" name="password" placeholder="entrez votre password">
+        
+        <button type="submit">LOGIN</button>
+        <button ><a href="ajoutercoach.php"> Inscrire </a> </button>
+    </form>
+</body>
+</html>
